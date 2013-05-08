d74f42d7fb183a2af2c03dc1b841775e51ecc145
Alex Ferreira <alex@dsol.com.br>
2013-05-08 16:02:55 -0300

    i18n

---
+++ ember/config/locales.js
@@ -0,0 +1,28 @@
+Ember.I18n.translations = {
+  "en": {
+    "locales": {
+      "en": "English",
+      "pt-BR": "Portuguese"
+    },
+    "select_locale": "Select Locale",
+    "app_name": "Polyglottous Emberjs",
+    "name": "name:",
+    "menu": {
+      "dashboard": "Dashboard",
+      "about": "About"
+    }
+  },
+  "pt-BR": {
+    "locales": {
+      "en": "English",
+      "pt-BR": "Portugues"
+    },
+    "select_locale": "Selecione o idioma",
+    "app_name": "Polyglottous Emberjs",
+    "name": "nome",
+    "menu": {
+      "dashboard": "Painel",
+      "about": "Sobre"
+    }
+  }
+}
