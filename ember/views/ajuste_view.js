480f528d2b59284020b34788f780515fc6deae9f
Alex Ferreira <alex@dsol.com.br>
2013-05-08 16:09:39 -0300

    Update Views and emberjs

--- ember/views/ajuste_view.js
+++ ember/views/ajuste_view.js
@@ -1,5 +1,7 @@
 var AjusteView = Ember.View.extend({
-
+  didInsertElement: function() {
+    this.$().hide().fadeToggle();
+  }
 });
 
 module.exports = AjusteView;
--- ember/views/sobre_view.js
+++ ember/views/sobre_view.js
@@ -1,5 +1,7 @@
 var SobreView = Ember.View.extend({
-
+  didInsertElement: function() {
+    this.$().hide().fadeToggle();
+  }
 });
 
 module.exports = SobreView;
