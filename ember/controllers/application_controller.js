3eb45ab0ad0f241c4c0fef9949dbd1a72c65a5ce
Alex Ferreira <alex@dsol.com.br>
2013-05-08 19:38:14 -0300

    Adding ga

---
+++ ember/controllers/application_controller.js
@@ -0,0 +1,29 @@
+var ApplicationController = Ember.Controller.extend({
+  currentPathChanged: function() {
+    var page;
+
+    // window.location gets updated later in the current run loop, so we will
+    // wait until the next run loop to inspect its value and make the call
+    // to track the page view
+    Ember.run.next(function() {
+      // Track the page in Google Analytics
+      if (!Ember.isNone(ga)) {
+        // Assume that if there is a hash component to the url then we are using
+        // the hash location strategy. Otherwise, we'll assume the history
+        // strategy.
+        page = window.location.hash.length > 0 ?
+               window.location.hash.substring(2) :
+               window.location.pathname;
+
+        // You need your Google Analytics code already loaded for _ga to be initialized
+        // ga.push(['_trackPageview', page]);
+
+        ga('set', 'page', page);
+        ga('send', 'pageview');
+      }
+    });
+  }.observes('currentPath')
+});
+
+module.exports = ApplicationController;
+
