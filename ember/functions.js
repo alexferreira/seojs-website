26ed048547b4038b7bdc76ab158c3acc23bd3bf2
Alex Ferreira <alex@dsol.com.br>
2013-05-09 02:00:42 -0300

    Extend jquery

--- ember/functions.js
+++ ember/functions.js
@@ -1,3 +1,18 @@
 jQuery(document).ready(function($) {
   
+  // check if element exist
+  if (!$.exist) {
+    $.extend({
+      exist: function(elm) {
+        if (typeof elm == null) return false;
+        if (typeof elm != "object") elm = $(elm);
+        return elm.length ? true : false;
+      }
+    });
+    $.fn.extend({
+      exist: function() {
+        return $.exist($(this));
+      }
+    });
+  }
 });
